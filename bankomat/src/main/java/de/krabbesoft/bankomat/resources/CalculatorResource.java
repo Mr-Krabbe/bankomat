69e462437c0993165063706071852b6deb3d5557
Christoph Krebbers <krabbesoft@gmail.com>
2023-11-25 10:36:39 +0100

    added swagger api annotations to the 'CalculatorResource'

--- bankomat/src/main/java/de/krabbesoft/bankomat/resources/CalculatorResource.java
+++ bankomat/src/main/java/de/krabbesoft/bankomat/resources/CalculatorResource.java
@@ -15,14 +15,36 @@ import javax.ws.rs.core.Response;
 import de.krabbesoft.bankomat.business.Bankomat;
 import de.krabbesoft.bankomat.business.InputParser;
 import de.krabbesoft.bankomat.model.Stueckelung;
+import de.krabbesoft.bankomat.resources.responses.ErrorMessageResponse;
 import de.krabbesoft.bankomat.resources.responses.StueckelungResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Path("stueckelung")
+@Tag(name = "Stückelung von Beträgen")
 public class CalculatorResource {
 
 	@Inject
 	private Bankomat bankomat;
 
+	@Operation(
+			summary = "Stückelt einen Betrag in Einzelwerte", 
+			description = "Übergeben wird der Betrag, der in Einzelwerte gestückelt werden soll. Der Gesamtbetrag wird "
+					+ "immer so gestückelt, dass die größtmöglichen Scheine bzw. Münzen zuerst genommen werden. "
+					+ "Damit wird erreicht, dass möglichst wenige Scheine und Münzen benötigt werden.", 
+			responses = {
+					@ApiResponse(
+							responseCode = "200", 
+							description = "Der Betrag konnte erfolgreich gestückelt werden", 
+							content = @Content(schema = @Schema(implementation = StueckelungResponse.class))),
+					@ApiResponse(
+							responseCode = "400", 
+							description = "Es liegt ein Fehler bei der Benutzereingabe vor", 
+							content = @Content(schema = @Schema(implementation = ErrorMessageResponse.class)))
+	})
 	@GET
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.APPLICATION_JSON)
